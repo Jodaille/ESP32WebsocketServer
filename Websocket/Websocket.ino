cd9345aae09d1bfef69e07553a951a170100a7f4
Jodaille <jody.noury@gmail.com>
2021-05-22 09:10:59 +0200

    websocket button with broadcast + json return

---
+++ Websocket/Websocket.ino
@@ -0,0 +1,216 @@
+/**
+ * ESP32 Websocket Server
+ * 
+ * use of SPIFFS storage for static (html, js) files
+ * 
+ * copied/pasted from :
+ * https://shawnhymel.com/1882/how-to-create-a-web-server-with-websockets-using-an-esp32-in-arduino/
+ */
+
+// Wifi configuration
+#include "config.h"
+
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsServer.h>
+#include <Arduino_JSON.h>
+
+// Constants
+const char *ssid = WIFI_SSID;
+const char *password =  WIFI_PASSWORD;
+const char *msg_toggle_led = "toggleLED";
+const char *msg_get_led = "getLEDState";
+const int dns_port = 53;
+const int http_port = 80;
+const int ws_port = 1337;
+const int red_led_pin = 33;
+const int led_pin = 4;
+// Globals
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(1337);
+char msg_buf[10];
+int led_state = 0;
+int red_led_state = 0;
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+/***********************************************************
+ * Functions
+ */
+
+// Callback: receiving any WebSocket message
+void onWebSocketEvent(uint8_t client_num,
+                      WStype_t type,
+                      uint8_t * payload,
+                      size_t length) {
+
+  // Figure out the type of WebSocket event
+  switch(type) {
+
+    // Client has disconnected
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", client_num);
+      break;
+
+    // New client has connected
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(client_num);
+        Serial.printf("[%u] Connection from ", client_num);
+        Serial.println(ip.toString());
+      }
+      break;
+
+    // Handle text messages from client
+    case WStype_TEXT:
+
+      // Print out raw message
+      Serial.printf("[%u] Received text: %s\n", client_num, payload);
+
+      // Toggle LED
+      if ( strcmp((char *)payload, "toggleLED") == 0 ) {
+        led_state = led_state ? 0 : 1;
+        Serial.printf("Toggling LED to %u\n", led_state);
+        digitalWrite(led_pin, led_state);
+
+      // Report the state of the LED
+      } else if ( strcmp((char *)payload, "getLEDState") == 0 ) {
+        sprintf(msg_buf, "%d", led_state);
+        Serial.printf("Sending to [%u]: %s\n", client_num, msg_buf);
+        webSocket.sendTXT(client_num, msg_buf);
+        webSocket.broadcastTXT(msg_buf);
+
+      } else if ( strcmp((char *)payload, "toggleRedLED") == 0 ) {
+        red_led_state = red_led_state ? 0 : 1;
+        Serial.printf("Toggling LED to %u\n", red_led_state);
+        digitalWrite(red_led_pin, red_led_state);
+
+      // Report the state of the LED
+      }
+      else if ( strcmp((char *)payload, "getRedLEDState") == 0 ) {
+        sprintf(msg_buf, "%d", red_led_state);
+        Serial.printf("Sending to [%u]: %s\n", client_num, msg_buf);
+        String json = getSensorReadings();
+        webSocket.sendTXT(client_num, json);
+        webSocket.broadcastTXT(json);
+
+      }else { // Message not recognized
+        Serial.println("[%u] Message not recognized");
+      }
+      break;
+
+    // For everything else: do nothing
+    case WStype_BIN:
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+    default:
+      break;
+  }
+}
+
+// Callback: send homepage
+void onIndexRequest(AsyncWebServerRequest *request) {
+  IPAddress remote_ip = request->client()->remoteIP();
+  Serial.println("[" + remote_ip.toString() +
+                  "] HTTP GET request of " + request->url());
+  request->send(SPIFFS, "/index.html", "text/html");
+}
+
+// Callback: send style sheet
+void onCSSRequest(AsyncWebServerRequest *request) {
+  IPAddress remote_ip = request->client()->remoteIP();
+  Serial.println("[" + remote_ip.toString() +
+                  "] HTTP GET request of " + request->url());
+  request->send(SPIFFS, "/style.css", "text/css");
+}
+void onSensorsRequest(AsyncWebServerRequest *request) {
+    String json = getSensorReadings();
+    request->send(200, "application/json", json);
+    json = String();
+}
+
+// Callback: send 404 if requested file does not exist
+void onPageNotFound(AsyncWebServerRequest *request) {
+  IPAddress remote_ip = request->client()->remoteIP();
+  Serial.println("[" + remote_ip.toString() +
+                  "] HTTP GET request of " + request->url());
+  request->send(404, "text/plain", "Not found");
+}
+
+String getSensorReadings(){
+
+  readings["sensor1"] = millis();
+  readings["message"] = "Hello";
+  readings["led_state"] = led_state ? 0 : 1;
+  readings["red_led_state"] = red_led_state ? 0 : 1;
+
+
+
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+/***********************************************************
+ * Main
+ */
+
+void setup() {
+  // Init LED and turn off
+  pinMode(led_pin, OUTPUT);
+  pinMode(red_led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
+
+  // Start Serial port
+  Serial.begin(115200);
+
+  // Make sure we can read the file system
+  if( !SPIFFS.begin()){
+    Serial.println("Error mounting SPIFFS");
+    while(1);
+  }
+
+  // Start access point
+  //WiFi.softAP(ssid, password);
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print our IP address
+  Serial.println();
+  //Serial.println("AP running");
+  Serial.print("My IP address: ");
+  //Serial.println(WiFi.softAPIP());
+
+  Serial.println(WiFi.localIP());
+
+  // On HTTP request for root, provide index.html file
+  server.on("/", HTTP_GET, onIndexRequest);
+
+  // On HTTP request for style sheet, provide style.css
+  server.on("/style.css", HTTP_GET, onCSSRequest);
+
+  server.on("/sensors", HTTP_GET, onSensorsRequest);
+
+
+  // Handle requests for pages that do not exist
+  server.onNotFound(onPageNotFound);
+
+  // Start web server
+  server.begin();
+
+  // Start WebSocket server and assign callback
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+
+}
+
+void loop() {
+
+  // Look for and handle WebSocket data
+  webSocket.loop();
+}
